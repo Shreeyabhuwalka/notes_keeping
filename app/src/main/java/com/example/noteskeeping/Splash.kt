c1be1f31289d21db70ce58731c0bb16d697172a8
Shreeya Bhuwalka <shreeyabhuwalka@gmail.com>
2022-01-19 00:19:58 +0530

    splash screen added

--- app/src/main/java/com/example/noteskeeping/Splash.kt
+++ app/src/main/java/com/example/noteskeeping/Splash.kt
@@ -1,11 +1,26 @@
 package com.example.noteskeeping
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
+@Suppress("DEPRECATION")
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
